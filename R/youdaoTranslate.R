9a8fbb908d87fc30e938d251c71039854f1ec073
Adrian Sun <sunyf20@mails.tsinghua.edu.cn>
2023-02-13 01:19:31 +0800

    update

--- R/youdaoTranslate.R
+++ R/youdaoTranslate.R
@@ -1,11 +1,26 @@
+#' youdaoTranslate
+#'
+#' `youdaoTranslate` is a free but limited tool to access Youdao Translator for word-level translation from Chinese to English
+#'
+#' @param text Text to be translated
 #' @import dplyr
 #' @import httr
 #' @import rvest
+#'
+#'
+#' @examples
+#'
+#' #  library(youdaoTranslate)
+#'
+#' #  youdaoTranslate(text = "民主")
+#'
+#'
+#' @export
 
-YDTranslate <- function(text) {
+youdaoTranslate <- function(text) {
   url <- paste0("http://dict.youdao.com/search?q=", text, "&keyfrom=dict.index")
   webpage <- read_html(url)
   result <- html_nodes(webpage, ".wt-container:nth-child(1) .title span") %>% html_text()
   result <- gsub("\n                ", "", result)
-  print(result)
+  result
 }
